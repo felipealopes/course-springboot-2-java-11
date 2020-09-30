db9c73d754321428b10f2c571f235d598339f86b
felipealopes <felipearaujolopes@gmail.com>
2020-09-30 14:03:58 -0300

    Category

---
+++ src/main/java/com/uniquesystems/course/repositories/CategoryRepository.java
@@ -0,0 +1,9 @@
+package com.uniquesystems.course.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.uniquesystems.course.entities.Category;
+
+public interface CategoryRepository  extends JpaRepository<Category, Long>{
+
+}
